0f1b8399fdfcfc18cfbe8bc7a19b427750028b9d
Aditya Dhar <adhar2032@gmsil.vom>
2021-10-28 10:47:55 -0700

    Added documentation

--- MatrixMultiplication/Program.cs
+++ MatrixMultiplication/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MatrixMultiplication
 {
@@ -6,7 +6,7 @@ namespace MatrixMultiplication
     {
         static void Main(string[] args)
         {
-
+            // Get Run Choice
             Console.WriteLine("Which system would you like to run?");
             Console.WriteLine("0 - Basic, 1 - Divide and Conquer, 2 - Strassen");
             Console.Write("Choice: ");
@@ -16,6 +16,7 @@ namespace MatrixMultiplication
             Console.Write("Choice: ");
             var runStyle = int.Parse(Console.ReadLine());
 
+            // Generate 20 Sets
             for (int exp = 0; exp < 20; exp++)
             {
                 var size = (int)Math.Pow(2, exp);
@@ -23,6 +24,7 @@ namespace MatrixMultiplication
                 double total = 0;
                 var count = 0;
 
+                // Run each set 1000 times (or once if the run time is simple)
                 for (int i = 0; i < (runStyle == 1 ? 1000 : 1); i++)
                 {
                     var ar1 = generateArrays(size);
@@ -37,11 +39,11 @@ namespace MatrixMultiplication
                             time = DivideAndConquerTestRun(ar1, ar2);
                         else if (response == 2)
                             time = StrassenTestRun(ar1, ar2);
-                        total += time;
-                        count++;
+                        total += time; //add time
+                        count++; // add to count
                     }
                 }
-                var avg = total / count;
+                var avg = total / count; // calculate average
                 if (avg == 0)
                     Console.WriteLine($"Execution Time For Size {size}: neglagable");
                 else
@@ -51,7 +53,7 @@ namespace MatrixMultiplication
         #region BasicMultiplication
         static double BasicMultiplicationTestRun(int[][] ar1, int[][] ar2)
         {
-            var watch = new System.Diagnostics.Stopwatch();
+            var watch = new System.Diagnostics.Stopwatch(); //create a stop watch to time the algorithm
             watch.Start();
             BasicMultiplication(ar1, ar2);
             watch.Stop();
@@ -63,6 +65,7 @@ namespace MatrixMultiplication
         {
             var result = new int[ar1.Length][];
 
+            //triple for loop to follow the formula
             for (int i = 0; i < ar1.Length; i++)
             {
                 result[i] = new int[ar1.Length];
@@ -82,7 +85,7 @@ namespace MatrixMultiplication
         #region Divide and Conquer
         static double DivideAndConquerTestRun(int[][] ar1, int[][] ar2)
         {
-            var watch = new System.Diagnostics.Stopwatch();
+            var watch = new System.Diagnostics.Stopwatch(); //create a stop watch to time the algorithm
             watch.Start();
             DivideAndConquer(ar1, ar2);
             watch.Stop();
@@ -94,11 +97,12 @@ namespace MatrixMultiplication
         {
             var result = InitializeMatrix(ar1.Length);
 
-            var newSize = ar1.Length / 2;
+            var newSize = ar1.Length / 2; //find the halfway point
 
             if (newSize <= 1)
-                return BasicMultiplication(ar1, ar2);
+                return BasicMultiplication(ar1, ar2); //Base Case
 
+            //Initialize all the new matricies
             int[][] A11 = InitializeMatrix(newSize);
             int[][] A12 = InitializeMatrix(newSize);
             int[][] A21 = InitializeMatrix(newSize);
@@ -108,19 +112,7 @@ namespace MatrixMultiplication
             int[][] B21 = InitializeMatrix(newSize);
             int[][] B22 = InitializeMatrix(newSize);
 
-            for (int i = 0; i < newSize; i++)
-            {
-                A11[i] = new int[newSize];
-                A12[i] = new int[newSize];
-                A21[i] = new int[newSize];
-                A22[i] = new int[newSize];
-
-                B11[i] = new int[newSize];
-                B12[i] = new int[newSize];
-                B21[i] = new int[newSize];
-                B22[i] = new int[newSize];
-            }
-
+            // Splitting algorithm
             for (int i = 0; i < newSize; i++)
                 for (int j = 0; j < newSize; j++)
                 {
@@ -134,16 +126,13 @@ namespace MatrixMultiplication
                     B22[i][j] = ar2[newSize + i][newSize + j];
                 }
 
-            //var C11 = AddMatricies(BasicMultiplication(A11, B11), BasicMultiplication(A12, B21));
-            //var C12 = AddMatricies(BasicMultiplication(A11, B12), BasicMultiplication(A12, B22));
-            //var C21 = AddMatricies(BasicMultiplication(A21, B11), BasicMultiplication(A22, B21));
-            //var C22 = AddMatricies(BasicMultiplication(A21, B12), BasicMultiplication(A22, B22));
-
+            //Recursively continue the formula
             var C11 = AddMatricies(DivideAndConquer(A11, B11), DivideAndConquer(A12, B21));
             var C12 = AddMatricies(DivideAndConquer(A11, B12), DivideAndConquer(A12, B22));
             var C21 = AddMatricies(DivideAndConquer(A21, B11), DivideAndConquer(A22, B21));
             var C22 = AddMatricies(DivideAndConquer(A21, B12), DivideAndConquer(A22, B22));
 
+            //Put it back together
             for (int i = 0; i < newSize; i++)
                 for (int j = 0; j < newSize; j++)
                 {
@@ -159,7 +148,7 @@ namespace MatrixMultiplication
         #region Strassen
         static double StrassenTestRun(int[][] ar1, int[][] ar2)
         {
-            var watch = new System.Diagnostics.Stopwatch();
+            var watch = new System.Diagnostics.Stopwatch(); //create a stop watch to time the algorithm
             watch.Start();
             Strassen(ar1, ar2);
             watch.Stop();
@@ -170,18 +159,13 @@ namespace MatrixMultiplication
         static int[][] Strassen(int[][] A, int[][] B)
         {
             if (A.Length <= 2)
-            {
-                //int[][] result = InitializeMatrix(1);
-                //result[0][0] = A[0][0] * B[0][0];
-                //return result;
-
-                return BasicMultiplication(A, B);
-            }
+                return BasicMultiplication(A, B); //Base Case
 
 
             int[][] C = InitializeMatrix(A.Length);
-            int k = A.Length / 2;
+            int k = A.Length / 2; //Find halfway point
 
+            //Initialize all the new matricies
             int[][] A11 = InitializeMatrix(k);
             int[][] A12 = InitializeMatrix(k);
             int[][] A21 = InitializeMatrix(k);
@@ -191,6 +175,7 @@ namespace MatrixMultiplication
             int[][] B21 = InitializeMatrix(k);
             int[][] B22 = InitializeMatrix(k);
 
+            // Splitting algorithm
             for (int i = 0; i < k; i++)
                 for (int j = 0; j < k; j++)
                 {
@@ -204,6 +189,7 @@ namespace MatrixMultiplication
                     B22[i][j] = B[k + i][k + j];
                 }
 
+            //Recursive algorithm
             int[][] P1 = Strassen(A11, SubtractMatricies(B12, B22));
             int[][] P2 = Strassen(AddMatricies(A11, A12), B22);
             int[][] P3 = Strassen(AddMatricies(A21, A22), B11);
@@ -217,6 +203,7 @@ namespace MatrixMultiplication
             int[][] C21 = AddMatricies(P3, P4);
             int[][] C22 = SubtractMatricies(SubtractMatricies(AddMatricies(P5, P1), P3), P7);
 
+            //Put it back together
             for (int i = 0; i < k; i++)
                 for (int j = 0; j < k; j++)
                 {
@@ -239,8 +226,8 @@ namespace MatrixMultiplication
                 for (int j = 0; j < ar1[0].Length; j++)
                 {
                     if (j == 0)
-                        result[i] = new int[ar1[0].Length];
-                    result[i][j] = ar1[i][j] + ar2[i][j];
+                        result[i] = new int[ar1[0].Length]; //New Row
+                    result[i][j] = ar1[i][j] + ar2[i][j]; //Add Values
                 }
             }
 
@@ -255,8 +242,8 @@ namespace MatrixMultiplication
                 for (int j = 0; j < ar1[0].Length; j++)
                 {
                     if (j == 0)
-                        result[i] = new int[ar1[0].Length];
-                    result[i][j] = ar1[i][j] - ar2[i][j];
+                        result[i] = new int[ar1[0].Length]; //New Row
+                    result[i][j] = ar1[i][j] - ar2[i][j]; //Subtract Values
                 }
             }
 
@@ -267,7 +254,7 @@ namespace MatrixMultiplication
             var result = new int[k][];
 
             for (int i = 0; i < result.Length; i++)
-                result[i] = new int[k];
+                result[i] = new int[k]; //generate rows
 
             return result;
         }
@@ -301,7 +288,7 @@ namespace MatrixMultiplication
                 var row1 = new int[SIZE];
                 for (int j = 0; j < row1.Length; j++)
                 {
-                    row1[j] = rnd.Next(1, 16);
+                    row1[j] = rnd.Next(1, 16); //create random number
                 }
 
                 array[i] = row1;
